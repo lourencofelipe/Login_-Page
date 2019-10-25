09c4eeb7ccdc5e99ebbaa7faecab4a8d8973fc29
Felipe <felipe.pucsc@gmail.com>
2019-10-25 06:42:16 -0300

    Adding login page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,19 @@
+import 'dart:ui';
 import 'package:flutter/material.dart';
+import 'pages/login.page.dart';
 
-void main();
+void main() => runApp(MyApp());
 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Login',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
+      ),
+      home: LoginPage(),
+    );
+  }
+}
---
+++ lib/pages/login.page.dart
@@ -0,0 +1,172 @@
+import 'package:flutter/material.dart';
+import 'dart:ui';
+
+import 'package:flutter/material.dart' as prefix0;
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.only(
+          top: 60,
+          left: 40,
+          right: 40,
+        ),
+        color: Colors.white,
+        child: ListView(
+          children: <Widget>[
+            SizedBox(
+              width: 128, //128
+              height: 128, //128
+              child: Image.asset("assets/logo.png"),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            TextFormField(
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(
+                labelText: "E-mail",
+                labelStyle: TextStyle(
+                  color: Colors.black38,
+                  fontWeight: FontWeight.w500,
+                  fontSize: 20,
+                ),
+              ),
+              style: TextStyle(
+                fontSize: 20,
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            TextFormField(
+              keyboardType: TextInputType.text,
+              obscureText: true,
+              decoration: InputDecoration(
+                  labelText: "Password",
+                  labelStyle: TextStyle(
+                    color: Colors.black38,
+                    fontSize: 20,
+                  )),
+              style: TextStyle(fontSize: 20),
+            ),
+            Container(
+              height: 40,
+              alignment: Alignment.centerRight,
+              child: FlatButton(
+                child: Text(
+                  "Forgot Password?",
+                  textAlign: TextAlign.right,
+                  style: TextStyle(
+                    color: Colors.black38,
+                  ),
+                ),
+                onPressed: () {},
+              ),
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            Container(
+              height: 60,
+              alignment: Alignment.centerLeft,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  stops: [0.3, 1],
+                  colors: [
+                    Color(0XFFF58524),
+                    Color(0XFFF92B7F),
+                  ],
+                ),
+                borderRadius: BorderRadius.all(
+                  Radius.circular(5),
+                ),
+              ),
+              child: SizedBox.expand(
+                child: FlatButton(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Text(
+                        "Login",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                          fontSize: 20,
+                        ),
+                        textAlign: TextAlign.left,
+                      ),
+                      Container(
+                        child: SizedBox(
+                          child: prefix0.Image.asset("assets/bone.png"),
+                          height: 28,
+                          width: 28,
+                        ),
+                      )
+                    ],
+                  ),
+                  onPressed: () => {},
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Container(
+              height: 60,
+              alignment: Alignment.centerLeft,
+              decoration: BoxDecoration(
+                color: Color(0xFF3C5A99),
+                borderRadius: BorderRadius.all(
+                  Radius.circular(5),
+                ),
+              ),
+              child: SizedBox.expand(
+                child: FlatButton(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Text(
+                        "Login com Facebook",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                          fontSize: 20,
+                        ),
+                        textAlign: TextAlign.left,
+                      ),
+                      Container(
+                        child: SizedBox(
+                          child: Image.asset("assets/fb-icon.png"),
+                          height: 28,
+                          width: 28,
+                        ),
+                      )
+                    ],
+                  ),
+                  onPressed: () {},
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Container(
+              height: 40,
+              child: FlatButton(
+                  child: Text(
+                    "Sign Up",
+                    textAlign: TextAlign.center,
+                  ),
+                  onPressed: () => {}),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
